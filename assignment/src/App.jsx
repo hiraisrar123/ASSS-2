d6ebbc36071dca09e4834dd20527b5a8d635d2c8
hiraisrar123 <hiraisrar47@gmail.com>
2026-01-21 21:14:25 +0500

    hh111

---
+++ assignment/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="hero">
+      <h1 className="logo">
+        Mar<span>é</span>e
+      </h1>
+
+      <p className="text">
+        Welcome to Marée, a brand new illustration & design portfolio
+        just waiting for your work to start populating its templates!
+      </p>
+
+      <button className="btn">PURCHASE</button>
+    </div>
+  );
+}
+
+export default App;
