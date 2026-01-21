3933072e4712b1fea18cba958983929740bb6345
hiraisrar123 <hiraisrar47@gmail.com>
2026-01-21 21:35:46 +0500

    ffdrwasdrfgh4

--- assignment/src/App.jsx
+++ assignment/src/App.jsx
@@ -3,22 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="hero">
-      <h1 className="logo">
-        Mar<span>é</span>e
-      </h1>
+    <>
+      <Navbar />
 
-      <p className="text">
-        Welcome to Marée, a brand new illustration & design portfolio
-        just waiting for your work to start populating its templates!
-      </p>
+      <div className="hero">
+        <h1 className="logo">
+          Mar<span>é</span>e
+        </h1>
 
-      <button className="btn">PURCHASE</button>
-    </div>
+        <p className="text">
+          Welcome to Marée, a brand new illustration & design portfolio
+          just waiting for your work to start populating its templates!
+        </p>
+
+        <button className="btn">PURCHASE</button>
+      </div>
+    </>
   );
 }
+console.log(App);
 
 export default App;
-
---
+++ assignment/src/assets/components/Navbar.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="nav-logo">Marée</div>
+
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>Portfolio</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
